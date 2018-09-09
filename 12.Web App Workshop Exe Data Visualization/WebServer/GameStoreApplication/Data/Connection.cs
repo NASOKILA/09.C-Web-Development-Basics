c422c29c2ec063a545a518bec4c7acf28260543d
NASOKILA <atanasskambitovv@gmail.com>
2018-09-10 00:01:35 +0300

    Web App Workshop Exe Data Visualization

---
+++ 12.Web App Workshop Exe Data Visualization/WebServer/GameStoreApplication/Data/Connection.cs
@@ -0,0 +1,7 @@
+﻿namespace HTTPServer.GameStoreApplication.Data
+{
+    public static class Connection
+    {
+        public const string ConnectionString = @"Data Source=.;Database=GameStoreDb;Integrated Security=True";
+    }
+}
